a472fdd37fe03a75c960734f9230fec228b0b3bb
Карина Дроздова <K.Drozd33@yandex.ru>
2020-05-14 22:31:00 +0300

    .

--- Task 6.1 Forms Dvumer/Form1.cs
+++ Task 6.1 Forms Dvumer/Form1.cs
@@ -16,5 +16,41 @@ namespace Task_6._1_Forms_Dvumer
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int n = Convert.ToInt32(numericUpDown1.Value);
+                int m = Convert.ToInt32(numericUpDown2.Value);
+                int[,] mass = new int[n, m];
+                Random r = new Random();
+                for (int i = 0; i < n; i++)
+                {
+                    for (int j = 0; j < m; j++)
+                    {
+                        mass[i, j] = r.Next(-50, 50);
+                        textBox1.Text += mass[i, j] + "       "+"\n\r"; 
+                    }
+                    textBox1.Text += "\n\r";
+                }
+                textBox1.Text += "\n\r";
+                for (int i = 1; i < n; i = i +2)
+                {
+                    for (int j = 0; j < m; j++)
+                    {
+                        if (mass[i, j] > 0)
+                        {
+                            textBox2.Text += mass[i, j] + "   ";
+                        }
+                    }
+                }
+            }
+            catch (Exception E)
+            {
+                textBox2.Text += E.Message;
+            }
+
+        }
     }
 }
